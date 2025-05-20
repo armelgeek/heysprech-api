6147e313236f69c40f1d3d98a52067a66486f363
Hajavololona Armellin <armelgeek5@gmail.com>
2025-05-21 00:22:56 +0300

    feat: download model

--- download_model.py
+++ download_model.py
@@ -1,9 +1,9 @@
 from transformers import MarianMTModel, MarianTokenizer
 
-model_name = "Helsinki-NLP/opus-mt-en-fr"
+model_name = "Helsinki-NLP/opus-mt-de-fr"
 model = MarianMTModel.from_pretrained(model_name)
 tokenizer = MarianTokenizer.from_pretrained(model_name)
 
 # Sauvegarde en local
-model.save_pretrained("./opus-mt-en-fr")
-tokenizer.save_pretrained("./opus-mt-en-fr")
+model.save_pretrained("./opus-mt-de-fr")
+tokenizer.save_pretrained("./opus-mt-de-fr")
