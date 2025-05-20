36de4c5ee7bed27b1443111d6f7709af9f310b78
Hajavololona Armellin <armelgeek5@gmail.com>
2025-05-21 02:39:36 +0300

    Update transcribe_api.py

--- transcribe_api.py
+++ transcribe_api.py
@@ -49,7 +49,6 @@ def transcrire_fichier(chemin_audio, repertoire_sortie):
         "--output_format", FORMAT_SORTIE,
         "--output_dir", repertoire_sortie,
         "--compute_type", "float32",
-        "--segment_resolution", 
         "--segment_resolution", "chunk",
         "--align_model", "WAV2VEC2_ASR_LARGE_LV60K_960H"
     ]
