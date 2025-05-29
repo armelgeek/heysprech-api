a0ee6d33d11d6ff1956290e6be4e6895d489d3ad
Hajavololona Armellin <armelgeek5@gmail.com>
2025-05-29 05:49:19 +0300

    feat: transcribe one file each

--- download_model.py
+++
@@ -1,9 +0,0 @@
-from transformers import MarianMTModel, MarianTokenizer
-
-model_name = "Helsinki-NLP/opus-mt-de-fr"
-model = MarianMTModel.from_pretrained(model_name)
-tokenizer = MarianTokenizer.from_pretrained(model_name)
-
-# Sauvegarde en local
-model.save_pretrained("./opus-mt-de-fr")
-tokenizer.save_pretrained("./opus-mt-de-fr")
