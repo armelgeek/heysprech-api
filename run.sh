6dd6920714455622a6e25e2fa1fd9bc6f65e8c95
Hajavololona Armellin <armelgeek5@gmail.com>
2025-06-01 11:26:03 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,16 +6,19 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# Récupère le chemin absolu du fichier audio
-AUDIO_PATH=$(realpath "$1")
-AUDIO_DIR=$(dirname "$AUDIO_PATH")
-AUDIO_FILE=$(basename "$AUDIO_PATH")
+# Création du répertoire sprech-audio à la racine
+sudo mkdir -p /sprech-audio
+sudo chmod 777 /sprech-audio
+
+# Récupère le chemin relatif du fichier audio par rapport au dossier courant
+AUDIO_FILE=$(basename "$1")
+AUDIO_DIR=$(dirname "$1")
 
 # Construction de l'image si elle n'existe pas
 docker build -t heysprech-api .
 
-# Exécution du conteneur
+# Exécution du conteneur avec le dossier sprech-audio à la racine
 docker run -it \
-    -v "$AUDIO_DIR":/workdir \
-    -v "$HOME/sprech-audio":/root/sprech-audio \
+    -v "$(pwd)/$AUDIO_DIR":/workdir \
+    -v /sprech-audio:/root/sprech-audio \
     heysprech-api "/workdir/$AUDIO_FILE"
